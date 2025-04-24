bfebfee13a7fe09d52ab5a9f8feeca109b9646b8
Raj Kumar Sah <84496051+rajkumarsah95@users.noreply.github.com>
2025-04-24 22:50:40 +0530

    Added Task 1 and Task 2

---
+++ Task1.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+add=b+a
+sub=b-a
+mul=b*a
+div=b/a 
+print("Addition: ",add)
+print("Subtraction: ",sub)
+print("Multiplication: ",mul)
+print("Division: ",div)
---
+++ Task2.py
@@ -0,0 +1,5 @@
+Fname=input("Enter your first name: ")
+Lname=input("Enter your last name: ")
+
+print(f"Hello, {Fname} {Lname}! Welcome to the python program." )
+
